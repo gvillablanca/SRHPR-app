911f10a8dfe6658917a3986f7fc82ad1835d6c50
wladi <wladimir2000@protonmail.com>
2023-09-14 12:13:18 -0300

    Se actualiza BDD segun diagrama de clases V2.

--- app/models.py
+++ app/models.py
@@ -1,24 +1,15 @@
 from django.db import models
 
 class Nacionalidad(models.Model):
-    nacionalidad = models.CharField(max_length=30, primary_key=True)
+    idNacionalidad = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
     pais = models.CharField(max_length=20)
-
-class Cliente(models.Model):
-    run_cliente = models.CharField(max_length=9, primary_key=True)
-    nacionalidad_nacionalidad = models.ForeignKey(Nacionalidad, on_delete=models.CASCADE)
-    primer_nombre = models.CharField(max_length=40)
-    segundo_nombre = models.CharField(max_length=40, blank=True, null=True)
-    apellido_paterno = models.CharField(max_length=40)
-    apellido_materno = models.CharField(max_length=40)
-    idioma = models.CharField(max_length=20)
-    telefono = models.IntegerField()
-    correo_electronico = models.CharField(max_length=50)
-    fecha_registro = models.DateTimeField()
 
 class Sucursal(models.Model):
     num_sucursal = models.AutoField(primary_key=True)
-    ubicacion = models.CharField(max_length=50)
+    direccion = models.CharField(max_length=50)
+    ciudad = models.CharField(max_length=50)
+    pais = models.CharField(max_length=30)
 
 class Habitacion(models.Model):
     num_habitacion = models.AutoField(primary_key=True)
@@ -29,31 +20,45 @@ class Habitacion(models.Model):
     piso = models.IntegerField()
     precio_dia = models.DecimalField(max_digits=6, decimal_places=0)
 
-class Reserva(models.Model):
-    habitacion_num_habitacion = models.IntegerField()
-    habitacion_sucursal_num_sucursal = models.IntegerField()
-    cliente_run_cliente = models.CharField(max_length=9)
-    fecha_inicio_reserva = models.DateTimeField()
-    fecha_termino_reserva = models.DateTimeField()
-    costo_total = models.DecimalField(max_digits=7, decimal_places=0)
-
 class Rol(models.Model):
-    cod_rol = models.CharField(max_length=10, primary_key=True)
-    cargo = models.CharField(max_length=40)
-    descripcion = models.CharField(max_length=1000)
+    cod_rol = models.CharField(max_length=20, primary_key=True)
+    titulo = models.CharField(max_length=80)
+    cargoDetalle = models.CharField(max_length=2000)
 
-class Trabajador(models.Model):
-    run_trabajador = models.CharField(max_length=9, primary_key=True)
-    rol_cod_rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
-    sucursal_num_sucursal = models.ForeignKey(Sucursal, on_delete=models.CASCADE)
+class Persona(models.Model):
+    id = models.AutoField(primary_key=True)
+    run = models.CharField(max_length=8)
+    cv = models.CharField(max_length=1)
     primer_nombre = models.CharField(max_length=40)
     segundo_nombre = models.CharField(max_length=40, blank=True, null=True)
     apellido_paterno = models.CharField(max_length=40)
     apellido_materno = models.CharField(max_length=40)
     correo_electronico = models.CharField(max_length=50)
-    salario = models.DecimalField(max_digits=7, decimal_places=0)
     telefono = models.IntegerField()
+    fecha_registro = models.DateTimeField()
+
+    class Meta:
+        abstract = True
+
+class Trabajador(Persona):
+    rol_cod_rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
+    sucursal_num_sucursal = models.ForeignKey(Sucursal, on_delete=models.CASCADE)
+    salario = models.DecimalField(max_digits=7, decimal_places=0)
+    clave = models.CharField(max_length=100)
 
     class Meta:
         unique_together = [('rol_cod_rol', 'sucursal_num_sucursal')]
+
+class Cliente(Persona):
+    nacionalidad_nacionalidad = models.ForeignKey(Nacionalidad, on_delete=models.CASCADE)
+    idioma = models.CharField(max_length=20)
+    clave = models.CharField(max_length=100)
+
+class Reserva(models.Model):
+    habitacion_num_habitacion = models.ForeignKey(Habitacion, on_delete=models.CASCADE)
+    habitacion_sucursal_num_sucursal = models.ForeignKey(Sucursal, on_delete=models.CASCADE)
+    idCliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    fecha_inicio_reserva = models.DateTimeField()
+    fecha_termino_reserva = models.DateTimeField()
+    costo_total = models.DecimalField(max_digits=7, decimal_places=0)
 
