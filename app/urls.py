702ff0f951e0fde6d4cc9821f40be80d42517d76
wladi <wladimir2000@protonmail.com>
2023-09-26 23:47:43 -0300

    Se crean views y urls, falta adaptar vistas a django.

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include	# include is used to include other urls.py files
-from .views import index
+from .views import index, cliente, soporte, recepcion, administracion
 
 urlpatterns = [
-  path('', index, name='index'),
+	path('', index, name='index'),
+	path('cliente/', cliente, name='cliente'),
+	path('soporte/', soporte, name='soporte'),
+	path('recepcion/', recepcion, name='recepcion'),
+	path('administracion/', administracion, name='administracion'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app/index.html')
+
+def cliente(request):
+		return render(request, 'app/cliente/home.html')
+
+def soporte(request):
+		return render(request, 'app/soporte/main.html')
+
+def recepcion(request):
+		return render(request, 'app/recepcion/main.html')
+
+def administracion(request):
+	return render(request, 'app/administracion/main.html')
